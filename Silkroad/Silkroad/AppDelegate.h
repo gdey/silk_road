7abdb55fc01b1b8c044e1f6e2dcdaeb31db97a48
Jason Koo <jalakoo@gmail.com>
2013-09-02 21:24:58 -0700

    Initial Code Commit
    SpriteKit XCode 5 Beta Project Using Template + Storyboard with Tests

---
+++ Silkroad/Silkroad/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Silkroad
+//
+//  Created by Jason Koo on 9/2/13.
+//  Copyright (c) 2013 SilkRoad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Silkroad/Silkroad/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  Silkroad
+//
+
+//  Copyright (c) 2013 SilkRoad. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
